3c48c2b581813da69012068c4e31006fdeb22efb
Co founders <projetawifv@gmail.com>
2022-02-17 14:21:08 +0100

    Initial commit

---
+++ PMS/PMS/PMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PMSApp.swift
+//  PMS
+//
+//  Created by m1 on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
